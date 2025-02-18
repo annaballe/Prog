5f7b19c0ca7b7fb485b40bfb399973965c14d4c4
Anna <130163340+annaballe@users.noreply.github.com>
2025-02-18 14:44:53 +0100

    Update sq_root.py

--- sq_root.py
+++ sq_root.py
@@ -1,15 +1,16 @@
 x= float (input ('Square root for what number?'))
 epsilon = 0.01
-num Guesses = 0
-IOW = 0.0
+numGuesses = 0
+low = 0.0
 high= max (1.0, x)
 ans= (high+ low) / 2.0
-while abs (ans ** 2-X)>= epsilon:
-print (low:, low, 'high , high, 'ans.", ans)
-numGuesses + = 1
-if ans t*2<x:
-low = ans
-else:
-high= ans
-ans = (high + low) | 2.0
-print (num Guesses:', hum Guesses) print (ans, 'is close to square root of, *).
+while abs (ans**2-x)>= epsilon:
+  print ('low:', low, 'high' , high, 'ans', ans)
+  numGuesses + = 1
+  if ans t*2<x:
+    low = ans
+  else:
+    high = ans
+  ans = (high + low)/2.0
+print (number of guesses:', numGuesses) 
+print (ans, 'is close to square root of', x )
